7b633d19619dd587ca31de21a0aebb94f8ea82bb
Vatsal Prajapati <prajapativatsal555@gmail.com>
2024-07-16 10:39:03 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+console.log(null > undefined);
+console.log(null == undefined);
+console.log(null >= undefined);
+
+console.log(undefined > null);
+console.log(undefined == null);
+console.log(undefined >= null);
