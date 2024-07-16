2f124039f610793e87567239d033f9803cb308a9
Vatsal Prajapati <prajapativatsal555@gmail.com>
2024-07-16 06:46:08 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1234;
+let accountEmail = "vatsal@google.com";
+var accountPassword = "123456";
+accountcity = "gujarat";
+
+//  const accountId = 3546;  not allowed
+//  let accountEmail = "sonu@gmail.com" 
+// var accountPassword = 234567;
+accountcity = "ahmedabad"
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountcity);
+console.table([accountId, accountEmail, accountPassword, accountcity])
