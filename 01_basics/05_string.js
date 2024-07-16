01de6353fe67a127c4f9c13946c4ba0590f747a8
Vatsal Prajapati <prajapativatsal555@gmail.com>
2024-07-16 13:52:39 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,24 @@
+const name = "vatsal"
+const repoCount = 50
+// console.log(name + repoCount + "prajapati");
+// console.log(`hello my name is ${name} and my repoCount is ${repoCount}`); //it is called string interpolation
+const gameName = new String ("DC_Rayuga")
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf("u"));
+let str1 = gameName.substring(0,4)
+let str2 = gameName.slice(-5,-2)
+// console.log(str1);
+// console.log(str2);
+let str3 = "         vatsal      "
+// console.log(str3);
+// console.log(str3.trim());
+let str4 = "vatsal maru"
+// console.log(str4);
+// console.log(str4.replace('maru','prajapati'));
+// console.log(str4.includes("vatsal"));
+console.log(gameName.split("_"));
