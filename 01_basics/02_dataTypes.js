6fc6e661017cb4425c07f036de619546c3bda6f2
Vatsal Prajapati <prajapativatsal555@gmail.com>
2024-07-16 08:49:17 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,2 +1,21 @@
 "use strict"; //treat all js as newer version
 // alert(3 + 3 ) // we are using nodejs, not browser
+console.log(3 
+    +
+     3) //code readability should be high
+
+     console.log("vatsal")
+
+     /* types of datatypes 
+        number => 2 to power 53
+        bigint
+        string => ""
+        boolean => true/false
+        null => standalone value
+        undefined => value not assinged to the variable
+        symbol => unique
+
+        object
+     */
+    console.log(typeof null);
+    console.log(typeof undefined);
