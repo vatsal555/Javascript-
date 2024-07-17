0e071ab12d99aa66ce0482f166b4bbf3f556a093
Vatsal Prajapati <prajapativatsal555@gmail.com>
2024-07-17 09:14:50 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,33 @@
+let arr1 = [1, 2, 3, 4, 5, 6]
+
+let wwe = ["roman", "undertaker", "brock", "triple h", "dean"]
+
+let arr = new Array("vatsal", 21 , "prajapati", 1, 2005)
+
+// console.log(arr1);
+// console.log(wwe);
+// console.log(arr);
+
+// Arrays Methods 
+
+// arr1.push(9)
+// arr1.pop()
+// arr1.unshift(5)
+// arr1.shift()
+// console.log(arr1);
+// console.log(arr1.includes(9));
+// console.log(arr1.indexOf(5));
+
+let myArr = arr1.join()
+// console.log(arr1);
+// console.log(myArr);
+
+// slice and splice
+
+console.log("A ", arr1);
+
+let myn1 = arr1.slice(1,3)
+console.log(myn1);
+let myn2 = arr1.splice(1,3)
+console.log("B ", arr1);
+console.log(myn2);
