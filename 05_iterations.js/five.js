e0007a826df19d606073de28e313ef5923f043ed
Vatsal Prajapati <prajapativatsal555@gmail.com>
2024-07-18 15:24:07 +0000

    for_each

---
+++ 05_iterations.js/five.js
@@ -0,0 +1,38 @@
+const coding = ["js ", "ruby", "java", "python"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// coding.forEach( (val) => {
+//     console.log(val);
+// } )
+
+function printMe (item) {
+    console.log(item);
+}
+
+// coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+    // console.log(item, index, arr);
+} )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+} )
