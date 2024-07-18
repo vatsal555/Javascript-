ad2d59a8f0ce69f8109cbc1d54f1ef4afca21c43
Vatsal Prajapati <prajapativatsal555@gmail.com>
2024-07-18 13:22:49 +0000

    while and do while loops

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,22 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is : ${index} `);
+//     index = index + 2
+// }
+
+// let myArr = ["roman", "seth", "dean", "brock"]
+// let arr = 0
+// while (arr < myArr.length) {
+//     console.log(`value is ${myArr[arr]}`);
+//     arr = arr + 1
+// }
+
+
+
+// let score = 0;
+let score = 11;
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <=10);
