22039ba36c83373175d225fcc69ae6933111aebb
Vatsal Prajapati <prajapativatsal555@gmail.com>
2024-08-30 05:04:18 +0000

    bmi calculator

--- bmi calculator/app.js
+++ bmi calculator/app.js
@@ -1,13 +1,20 @@
-const height = parseInt(document.querySelector("form #height"));
-const weight = parseInt(document.querySelector("form #weight"));
-const result = document.querySelector("form #results");
 const btn = document.querySelector("form button");
+const results = document.querySelector("#results");
 
-if (height === "" || height <= 0 || isNaN(height)) {
-  result.innerHTML = `please give a valid height ${height}`;
-} else if (weight === "" || weight <= 0 || isNaN(weight)) {
-  result.innerHTML = `please give a valid weight ${weight}`;
-} else {
-  const bmi = (weight / ((height * weight) / 10000)).toFixed(2);
-  result.innerText = `bmgi = ${bmi}`;
-}
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  resultPrinter();
+});
+
+let resultPrinter = () => {
+  const height = parseInt(document.querySelector("form #height").value);
+  const weight = parseInt(document.querySelector("form #weight").value);
+  if (height === "" || height <= 1 || isNaN(height)) {
+    results.innerHTML = `please give a valid height ${height}`;
+  } else if (weight === "" || weight <= 1 || isNaN(weight)) {
+    results.innerHTML = `please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    results.innerHTML = `${bmi}`;
+  }
+};
