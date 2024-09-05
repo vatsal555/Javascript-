c306bc0220260fdd0ff4e764ebb0b4d46531d594
Vatsal Prajapati <prajapativatsal555@gmail.com>
2024-09-05 04:04:32 +0000

    virtual keyboard

--- virtual_keyboard/app.js
+++ virtual_keyboard/app.js
@@ -1,18 +1,52 @@
 const button = document.querySelectorAll(".btn");
 const textarea = document.querySelector("textarea");
-const backSpace = document.querySelector(".backspace");
+const backSpace = document.querySelector(".back-space");
+const tab = document.querySelector(".tab");
+const capsLock = document.querySelector(".caps");
+const enter = document.querySelector(".enter");
 const shift = document.querySelector(".shift");
-const spaceBar = document.querySelector(".spacebar");
+const spaceBar = document.querySelector(".space-bar");
 
-// button.forEach((btn) => {
-//   btn.addEventListener("click", () => {
-//     console.log(btn.innerText);
-//   });
-// });
+let arr = [];
+
+let arr2 = [];
 
 for (const btn of button) {
+  //   console.log(btn);
   btn.addEventListener("click", () => {
-    console.log(btn.innerText);
     textarea.value += btn.innerText;
+    arr = textarea.value.split("");
+    console.log(arr);
   });
 }
+
+backSpace.addEventListener("click", () => {
+  arr.pop();
+  textarea.value = arr.join("");
+});
+
+tab.addEventListener("click", () => {
+  let length = arr.length;
+  arr.splice(length - 1, 0, " ");
+  // console.log(length);
+  // arr.splice()
+  textarea.value = arr.join("");
+});
+
+capsLock.addEventListener("click", () => {
+  for (const btn of button) {
+    btn.classList.toggle("upper");
+  }
+});
+
+enter.addEventListener("click", () => {
+  arr.push("");
+  textarea.value = arr.join("");
+});
+
+shift.addEventListener("click", () => {});
+
+spaceBar.addEventListener("click", () => {
+  arr.push(" ");
+  textarea.value = arr.join("");
+});
